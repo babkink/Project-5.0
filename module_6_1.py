e337df4166ea1e3b7ef34758f67ab572450f4207
babkink <babkink72@gmail.com>
2024-10-18 14:22:29 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,46 @@
+class Animal:
+    fed = True
+    alive = True
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}, но попробовал и умер")
+            self.alive = False
+
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    fed = False
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
